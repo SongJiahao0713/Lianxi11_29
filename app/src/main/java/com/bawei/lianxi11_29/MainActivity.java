052ede33e7e2e96841fd0deaa432d3be889c5f8c
Sjh123.4 <SongJiahao0713>
2019-11-29 19:41:25 +0800

    hahaha

--- app/src/main/java/com/bawei/lianxi11_29/MainActivity.java
+++ app/src/main/java/com/bawei/lianxi11_29/MainActivity.java
@@ -3,6 +3,7 @@ package com.bawei.lianxi11_29;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //主分支
+        Log.i("xxx","主分支");
     }
 }
